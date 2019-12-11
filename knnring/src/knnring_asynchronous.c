f015ab1f21a48cc4a3294dc513e3649627ce3696
Zikopis Evangelos <43642152+vagzikopis@users.noreply.github.com>
2019-12-11 15:22:29 +0200

    Update knnring_asynchronous.c

--- knnring/src/knnring_asynchronous.c
+++ knnring/src/knnring_asynchronous.c
@@ -122,15 +122,16 @@ knnresult distrAllkNN(double * X, int n, int d, int k)
   double local_min=DBL_MAX,local_max=-DBL_MAX;
   for(int i=0; i<result.m; i++)
   {
-    for(in j=1; j<=k; j=j+k-1)
+    for(int j=1; j<=k; j=j+k-1)
     {
       if (result.ndist[result.m*j+i] < local_min)
       {
         local_min = result.ndist[result.m*j+i];
       }
-    if (result.ndist[result.m*j+i] > local_max)
-    {
-      local_max = result.ndist[result.m*j+i];
+      if (result.ndist[result.m*j+i] > local_max)
+      {
+        local_max = result.ndist[result.m*j+i];
+      }
     }
   }
   double global_min, global_max;
